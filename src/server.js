e0921c849a9d63c8fb309410d41beeeb1e940fb0
MiguelCS05 <miguel_sc_santos@estudante.sesisenai.org.br>
2026-09-03 15:13:45 -0300

    pt1

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get("/",(req, res) => {
+    res.json("Hello World")
+})
+
+app.listen(port, () => {
+    console.log(`API rodando em: http://localhost:${port}`);
+})
